5a2236586889c38584209cb215a3c2521da5c577
Gi-ddo <60913804+Gi-ddo@users.noreply.github.com>
2022-04-20 00:16:34 +0200

    Code Clean UP Part 1
    - Made some folder changes
    - Cleaned up Player Controller Script

--- Phoenix/Assets/Scripts/Mechanics/PlayerController.cs
+++ Phoenix/Assets/Scripts/Mechanics/PlayerController.cs
@@ -5,11 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {   
+    [Header("Control Variables")]
     public static float movementSpeed;
     public float jumpForce;
-    //public float jumpForce;
-    private Rigidbody2D rb2d;
 
+    [Header("State Variables")]
     public bool fireC;
     public bool lightningC;
     public bool flowerC;
@@ -17,18 +17,22 @@ public class PlayerController : MonoBehaviour
 
     public Animator playerAnimator;
 
+    [Header("Reference GameObjects")]
     public GameObject flowerMcg, LightMcg, FireMcg;
     public GameObject lava, water, lightenig, phoenix;
     public GameObject sprint, jumpEF;
-    //public GameObject jumpEF;
-    private Vector3 characterscale;
+    
 
+    [Header("Controller Keys")]
     public KeyCode jumpP;
     public KeyCode faster;
     public KeyCode quit;
     public KeyCode restart;
 
-    // Start is called before the first frame update
+    private Vector3 characterscale;
+    private Rigidbody2D rb2d;
+
+
     void Start()
     {
         rb2d = this.gameObject.GetComponent<Rigidbody2D>();
@@ -38,26 +42,34 @@ public class PlayerController : MonoBehaviour
         lightningC = false;
         flowerC = false;
         canjump = true;
-
     }
 
-    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        Swimming();
+        Jump();
+    }
+
     void Update()
     {
-        //movement
-        var movement = Input.GetAxis("Horizontal");
+        var horizontalMovement = Input.GetAxis("Horizontal");
+        Movement(horizontalMovement);
+        FasterMovement();
+        SceneStuff();
+    }
+    void Movement(float movement)
+    {
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * movementSpeed;
-
         playerAnimator.SetFloat("Speed", Mathf.Abs(movement));
-        /////////////////////////////////////////////////////////////////////////////
+
         if (swimScript.isSwimming)
-        {         
+        {
             {
                 characterscale.y = 1;
             }
-            if ( movement < 0 )
+            if (movement < 0)
             {
-                characterscale.y = -1;    
+                characterscale.y = -1;
             }
             else if (movement > 0)
             {
@@ -72,7 +84,7 @@ public class PlayerController : MonoBehaviour
                 characterscale.x = -1;
 
             }
-            else if(movement > 0)
+            else if (movement > 0)
             {
                 characterscale.x = 1;
 
@@ -80,39 +92,88 @@ public class PlayerController : MonoBehaviour
             characterscale.y = 1;
         }
         transform.localScale = characterscale;
+    }
 
-        ////////////////////////////////////////////////////////////////////////////////////////
-        /*if (Input.GetAxis("Horizontal") < 0)
-        {
-            characterscale.x = -1;
-        }
-        if (Input.GetAxis("Horizontal") > 0)
-        {
-            characterscale.x = 1;
-        }
-        ///
-        if(swimScript.isSwimming == true)
-        {
-            characterscale.y = -1;
-        }
-        if (swimScript.isSwimming == true)
-        {
-            characterscale.y = 1;
-        }*/
-
-        //Speed
+    void FasterMovement()
+    {
         if (Input.GetKeyDown(faster))
         {
             movementSpeed = 9f;
-            playerAnimator.GetComponent<Animator>().speed = 3;
+            playerAnimator.speed = 3;
             sprint.SetActive(true);
         }
         else if (Input.GetKeyUp(faster))
         {
             movementSpeed = 6f;
-            playerAnimator.GetComponent<Animator>().speed = 1;
+            playerAnimator.speed = 1;
             sprint.SetActive(false);
+        } 
+    }
+
+    void Swimming()
+    {
+        if (swimScript.isSwimming == true)
+        {
+            playerAnimator.SetBool("Descending", false);
+            playerAnimator.SetBool("Jumping", false);
+            playerAnimator.SetBool("Swim", true);
         }
+        else if (swimScript.isSwimming == false)
+        {
+            playerAnimator.SetBool("Swim", false);
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(jumpP) && rb2d.velocity.y < 0.1f && canjump == true)
+        {
+            rb2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            playerAnimator.SetBool("Jumping", true);
+            playerAnimator.speed = 1;
+            isjumping = true;
+            playerAnimator.SetBool("Descending", false);
+            playerAnimator.SetBool("Swim", false);
+            canjump = false;
+            Invoke("canjumpagain", 1.1f);
+            phoenix.SetActive(true);
+            Invoke("PhoenixVisibility", 1f);
+            Instantiate(jumpEF, transform.position, Quaternion.identity);
+        }
+        
+        if (rb2d.velocity.y <= 0.1f && rb2d.velocity.y > -0.1f)
+        {
+            playerAnimator.SetBool("Jumping", false);
+        }
+        if (Input.GetKeyUp(jumpP))
+        {
+            swimScript.isSwimming = false;
+            playerAnimator.SetBool("Swim", false);
+        }
+
+        if (rb2d.velocity.y < -0.4f)
+        {
+            playerAnimator.SetBool("Descending", true);
+            playerAnimator.SetBool("Jumping", false);
+        }
+        if (rb2d.velocity.y > 0f)
+        {
+            playerAnimator.SetBool("Descending", false);
+        }
+    }
+    public void canjumpagain()
+    {
+        canjump = true;
+    }
+
+    public void PhoenixVisibility()
+    {
+        phoenix.SetActive(false);
+
+    }
+
+    public void SceneStuff()
+    {
         if (Input.GetKey(restart))
         {
             SceneManager.LoadScene(1);
@@ -121,6 +182,14 @@ public class PlayerController : MonoBehaviour
         {
             Debug.Log("youQuit");
             SceneManager.LoadScene(0);
+        }
+    }
+
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Untagged")
+        {
+            playerAnimator.SetBool("Descending", false);
         }
     }
 
@@ -152,76 +221,4 @@ public class PlayerController : MonoBehaviour
             Debug.Log("gotlight");
         }
     }
-
-    private void FixedUpdate()
-    {
-        //Swimming
-        if (swimScript.isSwimming == true)
-        {
-            playerAnimator.SetBool("Descending", false);
-            playerAnimator.SetBool("Jumping", false);
-            playerAnimator.SetBool("Swim", true);
-        }
-        else if(swimScript.isSwimming == false)
-        {
-            playerAnimator.SetBool("Swim", false);
-
-        }
-        //fall
-        if (rb2d.velocity.y < -0.4f)
-        {
-            playerAnimator.SetBool("Descending", true);
-            playerAnimator.SetBool("Jumping", false);
-        }
-        if (rb2d.velocity.y > 0f)
-        {
-            playerAnimator.SetBool("Descending", false);
-        }
-        //jump
-       if (Input.GetKeyDown(jumpP) && rb2d.velocity.y < 0.1f && canjump == true)
-        {
-            rb2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
-            playerAnimator.SetBool("Jumping", true);
-            playerAnimator.GetComponent<Animator>().speed = 1;
-            isjumping = true;
-            //Extras
-            playerAnimator.SetBool("Descending", false);
-            playerAnimator.SetBool("Swim", false);
-            canjump = false;
-            Invoke("canjumpagain", 1.1f);
-            phoenix.SetActive(true);
-            Invoke("PhoenixVisibility", 1f);
-            Instantiate(jumpEF, transform.position, Quaternion.identity);
-        }
-        //jump animation end
-        if (rb2d.velocity.y <= 0.1f && rb2d.velocity.y > -0.1f)
-        {
-            playerAnimator.SetBool("Jumping", false);
-        }
-       if(Input.GetKeyUp(jumpP))
-        {
-            swimScript.isSwimming = false;
-            playerAnimator.SetBool("Swim", false);
-
-        }
-    }
-    public void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.tag == "Untagged")
-        {
-            playerAnimator.SetBool("Descending", false);
-        }
-    }
-    public void canjumpagain()
-    {
-        canjump = true;
-    }
-
-    public void PhoenixVisibility()
-    {
-        phoenix.SetActive(false);
-     
-    }
-
-
 }
